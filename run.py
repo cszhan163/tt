dfded7a04be2e8dab919bbcd2ebd917b42647ed6
cszhan <cszhan163@example.com>
2018-06-14 13:02:31 +0800

    update ignore file

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     app.debug = app.config['DEBUG']
     ip = networkutil.getnetworkip()
     #sapp.config['MAIN_URL']="http://"+ip+"/"
-    app.run(ip)
+    app.run(ip,80)
